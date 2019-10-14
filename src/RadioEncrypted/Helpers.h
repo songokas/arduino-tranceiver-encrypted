c52865490f71f19702c026100cf81e96344c28d2
Tomas Jakstas <tom.jakstas@gmail.com>
2019-10-14 21:00:56 +0300

    reconnect helper added

---
+++ src/RadioEncrypted/Helpers.h
@@ -0,0 +1,29 @@
+#ifndef RADIO_ENCRYPTED_HELPERS
+#define RADIO_ENCRYPTED_HELPERS
+
+#include <RF24Mesh.h>
+#include <Streaming>
+
+namespace RadioEncrypted
+{
+
+    inline bool reconnect(RF24Mesh & mesh)
+    {
+        if (!mesh.checkConnection() ) {
+            Serial << F("Renewing Mesh Address") << endl;
+            if(!mesh.renewAddress(MESH_TIMEOUT)){
+                return mesh.begin(RADIO_CHANNEL, RF24_250KBPS, MESH_TIMEOUT);
+            } else {
+                return true;
+            }
+        } else {
+            auto currentAddress = mesh.getAddress(mesh.getNodeID());
+            if (!(currentAddress > 0)) {
+                Serial << F("Renew address: ") << currentAddress << endl;
+                return mesh.renewAddress(MESH_TIMEOUT);
+            }
+        }
+        return false;
+    }
+
+}
--- src/RadioEncrypted/RadioEncryptedConfig.h
+++ src/RadioEncrypted/RadioEncryptedConfig.h
@@ -11,8 +11,6 @@ const uint8_t MAX_LEN_MESSAGE {80};
 // encrypted message cipher text lenth
 const uint8_t MAX_LEN_ENCRYPTED_MESSAGE (MAX_LEN_TOPIC + MAX_LEN_MESSAGE);
 
-
-
 const uint8_t KEY_LENGH {16};
 const uint8_t VECTOR_LENGH {16};
 const uint16_t MESH_TIMEOUT {5000};
