95e17f93cc97b5e7b76fa5e873e3451b21999982
Tomas Jakstas <tom.jakstas@gmail.com>
2019-10-15 22:16:58 +0300

    random updated

--- src/RadioEncrypted/Entropy/AnalogSignalEntropy.cpp
+++ src/RadioEncrypted/Entropy/AnalogSignalEntropy.cpp
@@ -6,14 +6,14 @@ using RadioEncrypted::Entropy::AnalogSignalEntropy;
 
 AnalogSignalEntropy::AnalogSignalEntropy(uint8_t pin, uint16_t seed) : pin(pin)
 {
-    randomSeed(pin + seed);
+    randomSeed(random(2147483647L) ^ (pin + seed));
 }
 
 uint8_t AnalogSignalEntropy::randomByte()
 {
     uint8_t times = 8;
     while (times) {
-        randomSeed(random() ^ analogRead(pin));
+        randomSeed(random(2147483647L) ^ analogRead(pin));
         times--;
     }
     return (uint8_t)random(255);
