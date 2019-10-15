3c42da66a0ec9a3bd7ef0f047fe58a39c06370d1
Tomas Jakstas <tom.jakstas@gmail.com>
2019-10-15 22:04:27 +0300

    config update

--- src/RadioEncrypted/RadioEncryptedConfig.h
+++ src/RadioEncrypted/RadioEncryptedConfig.h
@@ -5,7 +5,7 @@
 
 // encrypted message cipher text lenth
 // does not work above 100 why ?
-const uint8_t MAX_LEN_ENCRYPTED_MESSAGE (MAX_LEN_TOPIC + MAX_LEN_MESSAGE);
+const uint8_t MAX_LEN_ENCRYPTED_MESSAGE 100
 
 const uint8_t KEY_LENGH {16};
 const uint8_t VECTOR_LENGH {16};
